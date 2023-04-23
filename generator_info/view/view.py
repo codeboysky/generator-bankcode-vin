cc5a0e8c8ff700da6ba7fc5574efee5b5986801a
daokun.lv <daokun.lv@dhc.com.cn>
2023-04-23 15:01:53 +0800

    添加银行切换功能

--- generator_info/view/view.py
+++ generator_info/view/view.py
@@ -2,10 +2,10 @@ from django.shortcuts import render
 from tools import luhn
 
 BANK_LIST = [
-    {"card_prefix": 622848, "card_name": "农行"},
-    {"card_prefix": 622848, "card_name": "工行"},
-    {"card_prefix": 621331, "card_name": "中行"},
-    {"card_prefix": 622188, "card_name": "邮储"},
+    {"card_prefix": "622848", "card_name": "农行"},
+    {"card_prefix": "622848", "card_name": "工行"},
+    {"card_prefix": "621331", "card_name": "中行"},
+    {"card_prefix": "622188", "card_name": "邮储"},
 ]
 
 
@@ -16,4 +16,4 @@ def my_view(request):
     result = []
     for i in range(0, 10):
         result.append({'card_num': luhn.gen_card_num(card_prefix, 16), 'vin': luhn.random_vin()})
-    return render(request, 'show.html', {'result': result,"bank_list":BANK_LIST,"card_prefix":card_prefix})
+    return render(request, 'show.html', {'result': result, "bank_list": BANK_LIST, "card_prefix": card_prefix})
